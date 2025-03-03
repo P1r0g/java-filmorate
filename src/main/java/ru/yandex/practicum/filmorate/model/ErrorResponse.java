946333ab361cdd8a8913ae953b8cd761a6a6ba01
P1r0G <pirogov27@bk.ru>
2025-03-03 21:11:06 +0300

    12 cпринт

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -1,13 +1,14 @@
 package ru.yandex.practicum.filmorate.handler;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
     }
-
 }
